e5d74cfb935ace2c1bc81efe3f5e7fb2c00afcdc
Tianhui Huang <t.tianhui.huang@gmail.com>
2023-10-07 20:01:42 -0700

    implemented registration skeleton

---
+++ src/main/java/dev/tianhui/movies/registration/RegistrationService.java
@@ -0,0 +1,13 @@
+package dev.tianhui.movies.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
